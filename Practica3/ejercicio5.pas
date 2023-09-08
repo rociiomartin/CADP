5a31e4b2dc8ca0651e3e16e9eaab0a45d8f7b0bc
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-08 15:24:39 -0300

    Create ejercicio5.pas

---
+++ Practica3/ejercicio5.pas
@@ -0,0 +1,8 @@
+{
+Realizar un programa que lea información de autos que están a la venta en una concesionaria. De cada auto se lee:
+marca, modelo y precio. La lectura finaliza cuando se ingresa la marca “ZZZ” que no debe procesarse. La
+información se ingresa ordenada por marca. Se pide calcular e informar:
+
+a. El precio promedio por marca.
+b. Marca y modelo del auto más caro.
+}
