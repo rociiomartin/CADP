eb738a758b28da0bae0eef31a42ed28394f34d00
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-08 14:12:47 -0300

    Create ejercicio1.pas

---
+++ Practica1/ejercicio1.pas
@@ -0,0 +1,9 @@
+{
+Realizar un programa que lea 2 números enteros desde teclado e informe en pantalla cuál de los dos números es
+el mayor.Si son iguales debei nformar enpantalla lo siguiente:“Los números leídos son iguales”.
+}
+program ejercicio1;
+begin
+  
+end.
+
