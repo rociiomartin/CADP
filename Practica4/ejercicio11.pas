8ea17568c563721471bfb686619232c668da1320
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-15 16:57:56 -0300

    Update ejercicio11.pas

--- Practica4/ejercicio11.pas
+++ Practica4/ejercicio11.pas
@@ -1,1 +1,11 @@
+{
+El colectivo de fotógrafos ArgenPics desea conocer los gustos de sus seguidores en las redes sociales. Para
+ello, para cada una de las 200 fotos publicadas en su página de Facebook, cuenta con la siguiente
+información: título de la foto, el autor de la foto, cantidad de Me gusta, cantidad de clics y cantidad de
+comentarios de usuarios. Realizar un programa que lea y almacene esta información. Una vez finalizada la
+lectura, el programa debe procesar los datos e informar:
 
+a) Título de la foto más vista (la que posee mayor cantidad de clics).
+b) Cantidad total de Me gusta recibidas a las fotos cuyo autor es el fotógrafo “Art Vandelay”.
+c) Cantidad de comentarios recibidos para cada una de las fotos publicadas en esa página.
+}
