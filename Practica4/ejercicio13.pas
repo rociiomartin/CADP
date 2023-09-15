c78881e433e7a39d1e384c233f881df9d35b6597
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-15 16:57:29 -0300

    Update ejercicio13.pas

--- Practica4/ejercicio13.pas
+++ Practica4/ejercicio13.pas
@@ -1,1 +1,11 @@
+{
+El Grupo Intergubernamental de Expertos sobre el Cambio Climático de la ONU (IPCC) realiza todos los
+años mediciones de temperatura en 100 puntos diferentes del planeta y, para cada uno de estos lugares,
+obtiene un promedio anual. Este relevamiento se viene realizando desde hace 50 años, y con todos los
+datos recolectados, el IPCC se encuentra en condiciones de realizar análisis estadísticos. Realizar un
+programa que lea y almacene los datos correspondientes a las mediciones de los últimos 50 años (la
+información se ingresa ordenada por año). Una vez finalizada la carga de la información, obtener:
 
+a) El año con mayor temperatura promedio a nivel mundial.
+b) El año con la mayor temperatura detectada en algún punto del planeta en los últimos 50 años.
+}
