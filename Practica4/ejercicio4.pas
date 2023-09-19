60526cf92b6de3e89e220d03f0af0ef8dcec36fd
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-19 01:21:57 -0300

    Create ejercicio4.pas

---
+++ Practica4/ejercicio4.pas
@@ -0,0 +1,11 @@
+{
+ Se dispone de un vector con 100 números enteros. Implementar los siguientes módulos:
+a) posicion: dado un número X y el vector de números, retorna la posición del número X en dicho vector,
+o el valor -1 en caso de no encontrarse.
+b) intercambio: recibe dos valores x e y (entre 1 y 100) y el vector de números, y retorna el mismo vector
+donde se intercambiaron los valores de las posiciones x e y.
+c) sumaVector: retorna la suma de todos los elementos del vector.
+d) promedio: devuelve el valor promedio de los elementos del vector.
+e) elementoMaximo: retorna la posición del mayor elemento del vector
+f) elementoMinimo: retorna la posicion del menor elemento del vector
+}
