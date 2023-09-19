33daedd661f8bd8696b824f30d84bc057674b335
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-19 00:46:34 -0300

    Update ejercicio4.pas

--- Practica4-2/ejercicio4.pas
+++ Practica4-2/ejercicio4.pas
@@ -1,9 +1,14 @@
 {
-Realice un programa que resuelva los siguientes incisos:
-a. Lea nombres de alumnos y los almacene en un vector de a lo sumo 500 elementos. La lectura finaliza
-cuando se lee el nombre ‘ZZZ’, que no debe procesarse.
-b. Lea un nombre y elimine la primera ocurrencia de dicho nombre en el vector.
-c. Lea un nombre y lo inserte en la posición 4 del vector.
-d. Lea un nombre y lo agregue al vector.
-Nota: Realizar todas las validaciones necesarias.
+Una cátedra dispone de información de sus alumnos (a lo sumo 1000). De cada alumno se conoce nro de
+alumno, apellido y nombre y cantidad de asistencias a clase. Dicha información se encuentra ordenada por
+nro de alumno de manera ascendente. Se pide:
+a. Un módulo que retorne la posición del alumno con un nro de alumno recibido por parámetro. El
+alumno seguro existe.
+b. Un módulo que reciba un alumno y lo inserte en el vector.
+c. Un módulo que reciba la posición de un alumno dentro del vector y lo elimine.
+d. Un módulo que reciba un nro de alumno y elimine dicho alumno del vector
+e. Un módulo que elimine del vector todos los alumnos con cantidad de asistencias en 0.
+Nota: Realizar el programa principal que invoque los módulos desarrollados en los incisos previos con datos
+leídos de teclado.
+
 }
