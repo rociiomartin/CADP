b26716e43eb177c338ac090c9b8a1f4260722e62
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-19 00:45:53 -0300

    Update ejercicio3.pas

--- Practica4/ejercicio3.pas
+++ Practica4/ejercicio3.pas
@@ -1,1 +1,15 @@
-
+{
+Una empresa de transporte de caudales desea optimizar el servicio que brinda a sus clientes. Para ello,
+cuenta con información sobre todos los viajes realizados durante el mes de marzo. De cada viaje se cuenta
+con la siguiente información: día del mes (de 1 a 31), monto de dinero transportado y distancia recorrida
+por el camión (medida en kilómetros).
+a. Realizar un programa que lea y almacene la información de los viajes (a lo sumo 200). La lectura finaliza
+cuando se ingresa una distancia recorrida igual a 0 km, que no debe procesarse.
+b. Realizar un módulo que reciba el vector generado en a) e informe:
+- El monto promedio transportado de los viajes realizados
+- La distancia recorrida y el día del mes en que se realizó el viaje que transportó menos dinero.
+- La cantidad de viajes realizados cada día del mes.
+c. Realizar un módulo que reciba el vector generado en a) y elimine todos los viajes cuya distancia
+recorrida sea igual a 100 km.
+Nota: para realizar el inciso b, el vector debe recorrerse una única vez
+}
