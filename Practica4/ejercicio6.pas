f623ce98ce73b6628e3f36db87c0e43d3f515bda
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-19 01:22:53 -0300

    Update ejercicio6.pas

--- Practica4/ejercicio6.pas
+++ Practica4/ejercicio6.pas
@@ -1,1 +1,5 @@
-
+{
+Dado que en la solución anterior se recorre dos veces el vector (una para calcular el elemento máximo y
+otra para el mínimo), implementar un único módulo que recorra una única vez el vector y devuelva ambas
+posiciones.
+}
