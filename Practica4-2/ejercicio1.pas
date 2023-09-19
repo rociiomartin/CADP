b009e69738887cd6bd4d6a27bce781d26802a05c
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-19 01:19:40 -0300

    Create ejercicio1.pas

---
+++ Practica4-2/ejercicio1.pas
@@ -0,0 +1,6 @@
+{
+ a. Dado un vector de enteros de a lo sumo 500 valores, realice un módulo que reciba dicho vector y un valor
+n y retorne si n se encuentra en el vector o no.
+b. Modifique el módulo del inciso a. considerando ahora que el vector se encuentra ordenado de manera
+ascendente.
+}
