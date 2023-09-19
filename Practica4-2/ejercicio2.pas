13f3f79ae13d023e5304aef70731fe617680a405
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-19 01:16:08 -0300

    Update and rename ejercicio2.pas to ejercicio2.pas

---
+++ Practica4-2/ejercicio2.pas
@@ -0,0 +1,9 @@
+{
+Realice un programa que resuelva los siguientes incisos:
+a. Lea nombres de alumnos y los almacene en un vector de a lo sumo 500 elementos. La lectura finaliza
+cuando se lee el nombre ‘ZZZ’, que no debe procesarse.
+b. Lea un nombre y elimine la primera ocurrencia de dicho nombre en el vector.
+c. Lea un nombre y lo inserte en la posición 4 del vector.
+d. Lea un nombre y lo agregue al vector.
+Nota: Realizar todas las validaciones necesarias.
+}
