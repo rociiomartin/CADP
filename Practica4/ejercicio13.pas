2d7d6b1362a7510ae7432757f21d764723ba84ca
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-20 16:56:24 -0300

    Update ejercicio13.pas

--- Practica4/ejercicio13.pas
+++ Practica4/ejercicio13.pas
@@ -27,44 +27,73 @@ const
     TEMP=5; DF=3;
 type
     rango1=1..TEMP;
-    rango2=1..50;//en teoria seria 1973..2023
-    temp=record     
-            año:integer; //ordenada x este campo
-            temp:integer;
-         end;
-         
-    vectorTemp = array[rango1] of temp;
-    vectorAño = array[rango2] of vectorTemp;
+    rango2=1..DF;//en teoria seria 1973..2023
+
+    vectorTemp = array [rango1] of real;
+    vectorAnio = array [rango2] of vectorTemp;
 //PROCESOS
-procedure CargarVectorAño(var v:vectorAño; v:vectorTemp);
-    LeerTemperatura(var t:temp);
+procedure CargarVectorAnio(var v:vectorAnio);
+    procedure CargarVectorTemp(var v:vectorTemp);
+    var
+        i:rango1;
+        t:real;
     begin
-        writeln('Ingrese el año');
-        readln(t.año);
-        writeln('Ingrese la temperatura del lugar');
-        readln(t.temp);
+        writeln('---Ingrese temperaturas---');
+        for i:= 1 to TEMP do
+        begin
+            readln(t); 
+            v[i]:=t;
+        end;
     end;
 var
-    t:temperatura;añoActual:integer;
-    i:rango1;
+    vec:vectorTemp;
+    i:rango2;
 begin
     for i:= 1 to DF do
     begin 
-        LeerTemperatura(t);
-        añoActual:=t.año;
-        while(t.año = añoActual)do
+        CargarVectorTemp(vec);
+        v[i]:= vec;
+    end;
+end;
+procedure RecorrerVectores(v:vectorAnio);
+    procedure Maximo(var i:rango2; var max:real;cant:real;anio:rango2);
+    begin
+        if(cant > max)then
         begin
-            for i:=1 to TEMP do
-            begin
-                v[añoActual]
-            end;
+            max:=cant;
+            i:=anio;
         end;
     end;
+    procedure TempPromedio(v:vectorTemp;a:rango2; var anio:rango2; var max,tempPro:real);
+    var
+        i:rango1;
+        prome:real;
+    begin
+        prome:=0;
+        for i:=1 to TEMP do 
+        begin
+            prome:=prome+v[i];
+            Maximo(anio,max,v[i],a);
+        end;
+        tempPro:=prome/TEMP;
+    end;
+var
+    i,anio1,anio2:rango2;
+    max1,max2,tempPro:real;
+begin
+    max1:=-1;max2:=-1;
+    for i:=1 to DF do
+    begin
+        TempPromedio(v[i],i,anio1,max1,tempPro);
+        Maximo(anio2,max2,tempPro,i);
+    end;
+    writeln('El año con la mayor temperatura detectada en algún punto del planeta en los últimos 50 años: ', anio1);
+    writeln('El año con mayor temperatura promedio a nivel mundial: ', anio2);
 end;
 //PP
 var
-    v:vectorTemp;
-    vec:vectorAño;
+    vec:vectorAnio;
 begin
-    CargarVectorTemp(v);
+    CargarVectorAnio(vec);
+    RecorrerVectores(vec);
 end.
