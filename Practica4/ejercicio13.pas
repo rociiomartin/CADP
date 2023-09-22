a4c781ec4b7e95c5a51d9bad7cff048dde21ad98
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-22 16:21:42 -0300

    Update ejercicio13.pas

--- Practica4/ejercicio13.pas
+++ Practica4/ejercicio13.pas
@@ -10,18 +10,6 @@ a) El año con mayor temperatura promedio a nivel mundial.
 b) El año con la mayor temperatura detectada en algún punto del planeta en los últimos 50 años.
 }
 
-{
-El Grupo Intergubernamental de Expertos sobre el Cambio Climático de la ONU (IPCC) realiza todos los
-años mediciones de temperatura en 100 puntos diferentes del planeta y, para cada uno de estos lugares,
-obtiene un promedio anual. Este relevamiento se viene realizando desde hace 50 años, y con todos los
-datos recolectados, el IPCC se encuentra en condiciones de realizar análisis estadísticos. Realizar un
-programa que lea y almacene los datos correspondientes a las mediciones de los últimos 50 años (la
-información se ingresa ordenada por año). Una vez finalizada la carga de la información, obtener:
-
-a) El año con mayor temperatura promedio a nivel mundial.
-b) El año con la mayor temperatura detectada en algún punto del planeta en los últimos 50 años.
-}
-
 program ejercicio13;
 const
     TEMP=100; DF=50;
