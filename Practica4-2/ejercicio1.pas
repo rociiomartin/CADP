e1897a991305d7c50233a5704388ec3e12f3a8cf
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-22 16:22:13 -0300

    Update ejercicio1.pas

--- Practica4-2/ejercicio1.pas
+++ Practica4-2/ejercicio1.pas
@@ -4,21 +4,7 @@ n y retorne si n se encuentra en el vector o no.
 b. Modifique el módulo del inciso a. considerando ahora que el vector se encuentra ordenado de manera
 ascendente.
 }
-{
- a. Dado un vector de enteros de a lo sumo 500 valores, realice un módulo que reciba dicho vector y un valor
-n y retorne si n se encuentra en el vector o no.
-b. Modifique el módulo del inciso a. considerando ahora que el vector se encuentra ordenado de manera
-ascendente.
-}                                    
-
-{
- a. Dado un vector de enteros de a lo sumo 500 valores, realice un módulo que reciba dicho vector y un valor
-n y retorne si n se encuentra en el vector o no.
-b. Modifique el módulo del inciso a. considerando ahora que el vector se encuentra ordenado de manera
-ascendente.
-}                                    
-
-
+                             
 program ejercicio1;
 const 
     DF=2; //a lo sumo
