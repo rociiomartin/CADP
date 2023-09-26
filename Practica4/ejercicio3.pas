191decf5e36fcf4c430a40d104f64d7e7e42c4e6
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-26 16:08:38 -0300

    Update ejercicio3.pas

--- Practica4/ejercicio3.pas
+++ Practica4/ejercicio3.pas
@@ -9,3 +9,94 @@ posición X hasta la Y. Asuma que tanto X como Y son menores o igual a la dimens
 que, dependiendo de los valores de X e Y, podría ser necesario recorrer hacia adelante o hacia atrás.
 e) Utilizando el módulo implementado en el inciso anterior, vuelva a realizar los incisos a, b y c.
 }
+
+program ejercicio3;
+const
+  dimF = 10;  { Dimensión física del vector }
+type
+  rango=1..dimF;
+  vector = array[rango] of integer; //se dispone
+
+//PROCESOS
+procedure CargarVector(var v:vector; var dimL:integer);//se dispon
+var
+    num:integer;
+begin
+  dimL := 0;
+  writeln('Ingrese numeros enteros'); //no se sabe el corte d control
+  while(dimL < dimF) do
+  begin
+    read(num);
+    diml:=diml+1;
+    v[diml] := num;
+  end;
+end;
+//-------------
+procedure IncisoA(v: vector; dimL: integer);
+var
+  i:rango;
+begin
+  for i := 1 to dimL do
+    writeln('Elemento ', i, ': ', v[i]);
+end;
+//--------------
+procedure IncisoB(v: vector; dimL: integer);
+var
+ i:rango;
+begin
+  for i := dimL downto 1 do
+    writeln('Elemento ', i, ': ', v[i]);
+end;
+//--------------
+procedure IncisoC(v: vector; dimL: integer);
+var 
+    i:rango;
+    aux:integer;
+begin
+  aux:= dimL div 2;
+  for i := aux downto 1 do
+    writeln('Elemento ', i, ': ', v[i]);
+  aux:=aux+1;
+  for i := aux to diml do
+    writeln('Elemento ', i, ': ', v[i]); 
+end;
+//--------------
+procedure IncisoD(v: vector; dimL,X,Y: integer);
+var
+ i:rango;
+begin
+  if (X > Y)then
+      for i := X downto Y do
+        writeln('Elemento ', i, ': ', v[i])
+  else
+      for i := X to Y do
+        writeln('Elemento ', i, ': ', v[i]);
+end;
+//--------------
+procedure IncisoE(v: vector; dimL: integer);
+var
+  i:rango;
+  X,Y:integer;
+begin
+  writeln('Ingrese X e Y');
+  readln(X, Y);
+  IncisoD(v, dimL,X,Y);
+  IncisoA(v, dimL);
+  IncisoB(v, dimL);
+  IncisoC(v, dimL);
+end;
+
+//PP
+var
+  v: vector;
+  dimL,X,Y: integer;
+begin
+  CargarVector(v,dimL); //se dispone
+  IncisoA(v, dimL);
+  IncisoB(v, dimL);
+  IncisoC(v, dimL);
+  writeln('Ingrese X e Y');
+  readln(X, Y);
+  IncisoD(v, dimL,X,Y);
+  IncisoE(v, dimL);
+end.
