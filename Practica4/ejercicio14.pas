cc8cca4da5e188f198f5fde7d83108630b92f739
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-09-26 16:20:33 -0300

    Update ejercicio14.pas

--- Practica4/ejercicio14.pas
+++ Practica4/ejercicio14.pas
@@ -1,1 +1,31 @@
+{
+}
+program ejercicio14;
+const
+  DF = 5;  
+type
+  rango=1..DF;
+  rango2=1..5;
+  rango3=1..100;
+  
+  
+  
+  tabla= array [rango] of integer; //se dispone
+
+//PROCESOS
+procedure CargarVector(var v:vector);//se dispon
+var
+    num:integer;
+begin
+  writeln('Ingrese numeros enteros'); //no se sabe el corte d control
+  while(num <> FIN) do
+  begin
+    read(num);
+    diml:=diml+1;
+    v[diml] := num;
+  end;
+end;
+
+//pp
+
 
