be4d493c7b678ff46e497f63eeb4663e8fd0a19f
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-10 15:31:25 -0300

    Create ejercicio7.pas

---
+++ Practica5/ejercicio7.pas
@@ -0,0 +1,9 @@
+{
+El Programa Horizonte 2020 (H2020) de la Unión Europea ha publicado los criterios para financiar
+proyectos de investigación avanzada. Para los proyectos de sondas espaciales vistos en el ejercicio anterior, se
+han determinado los siguientes criterios:
+
+- sólo se financiarán proyectos cuyo costo de mantenimiento no supere el costo de construcción
+- no se financiarán proyectos espaciales que analicen ondas de radio, ya que esto puede realizarse desde la
+superficie terrestre con grandes antenas.
+}
