0dc91d7abf097577b6a69f764a45cbba077bb6c3
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-10 15:30:58 -0300

    Create ejercicio6.pas

---
+++ Practica5/ejercicio6.pas
@@ -0,0 +1,21 @@
+{
+La Agencia Espacial Europea (ESA) está realizando un relevamiento de todas las sondas espaciales lanzadas
+al espacio en la última década. De cada sonda se conoce su nombre, duración estimada de la misión
+(cantidad de meses que permanecerá activa), el costo de construcción, el costo de mantenimiento mensual y
+rango del espectro electromagnético sobre el que realizará estudios. Dicho rango se divide en 7 categorías:
+
+1. radio; 2. microondas; 3.infrarrojo; 4.luz visible; 5. ultravioleta; 6. rayos X; 7. rayos gamma
+Realizar un programa que lea y almacene la información de todas las sondas espaciales. La lectura finaliza al
+ingresar la sonda llamada “GAIA”, que debe procesarse.
+
+Una vez finalizada la lectura, informar:
+
+a. El nombre de la sonda más costosa (considerando su costo de construcción y de mantenimiento).
+
+b. La cantidad de sondas que realizarán estudios en cada rango del espectro electromagnético.
+
+€. La cantidad de sondas cuya duración estimada supera la duración promedio de todas las sondas.
+
+d. El nombre de las sondas cuyo costo de construcción supera el costo promedio entre todas las sondas.
+Nota: para resolver los incisos a), b), c) y d), la lista debe recorrerse la menor cantidad de veces posible
+}
