7a30eca4e4fed301973f4c462770199c540fcd4e
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-17 16:59:17 -0300

    Create ejercicio10.pas

---
+++ Practica6/ejercicio10.pas
@@ -0,0 +1,11 @@
+{
+10. Una empresa de sistemas está desarrollando un software para organizar listas de espera de clientes. Su
+funcionamiento es muy sencillo: cuando un cliente ingresa al local, se registra su DNI y se le entrega un
+número (que es el siguiente al último número entregado). El cliente quedará esperando a ser llamado por su
+número, en cuyo caso sale de la lista de espera. Se pide:
+
+a. Definir una estructura de datos apropiada para representar la lista de espera de clientes.
+
+b. Implementar el módulo RecibirCliente, que recibe como parámetro el DNI del cliente y la lista de clientes
+en espera. asigna un número al cliente v retorna la lista de espera actualizada.
+}
