79e4775c1c6896401037621b6932e264624ba2bb
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-17 16:38:32 -0300

    Update ejercicio8.pas

--- Practica6/ejercicio8.pas
+++ Practica6/ejercicio8.pas
@@ -2,3 +2,21 @@
 Utilizando el programa del ejercicio 1, modificar el módulo armarNodo para que los elementos de la lista
 queden ordenados de manera ascendente (insertar ordenado).
 }
+
+procedure armarNodo(var L: lista; v: integer);
+var
+        aux, ant, act : lista;
+begin
+        act:=L;
+        ant:=L;
+        new(aux);
+
+        aux^.n := v;
+        while (act<>nil) AND (act^.n > v) do begin
+                ant:=act;
+                act:=L^.sig;
+        end;
+        if act = L then L:=aux
+        else ant^.sig:=aux;
+        aux^.sig:=act;
+end;
