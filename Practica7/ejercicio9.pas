1c6ef4e77956836ce2109702cd77aac159333a76
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-20 16:56:51 -0300

    Create ejercicio9.pas

---
+++ Practica7/ejercicio9.pas
@@ -0,0 +1,19 @@
+{
+Un cine posee la lista de películas que proyectará durante el mes de Febrero. De cada película se
+tiene: código de película, título de la película, código de género (1: acción, 2: aventura, 3: drama, 4:
+suspenso, 5: comedia, 6: bélica, 7: documental y 8: terror) y puntaje promedio otorgado por las
+críticas. Dicha estructura no posee orden alguno.
+
+Se pide:
+
+a) Actualizar (en la lista que se dispone) el puntaje promedio otorgado por las críticas. Para ello se
+debe leer desde teclado las críticas que han realizado críticos de cine, de cada crítica se lee: DNI
+del crítico, apellido y nombre del crítico, código de película y el puntaje otorgado. La lectura
+finaliza cuando se lee el código de película -1 y la información viene ordenada por código de
+película.
+b) Informar el código de género que más puntaje obtuvo entre todas las críticas.
+c) Informar el apellido y nombre de aquellos críticos que posean la misma cantidad de dígitos pares
+que impares en su DNI.
+d) Realizar un módulo que elimine de la lista que se dispone una película cuyo código se recibe
+como parámetro (el mismo puede no existir).
+}
