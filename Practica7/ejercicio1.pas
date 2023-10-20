e5cac7174e53b9341cd53266e8ae44e0a4b2c337
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-20 16:30:19 -0300

    Update ejercicio1.pas

--- Practica7/ejercicio1.pas
+++ Practica7/ejercicio1.pas
@@ -1,5 +1,5 @@
 {
-1. Una productora nacional realiza un casting de personas para la selección de actores extras de una
+Una productora nacional realiza un casting de personas para la selección de actores extras de una
 nueva película, para ello se debe leer y almacenar la información de las personas que desean
 participar de dicho casting. De cada persona se lee: DNI, apellido y nombre, edad y el código de
 género de actuación que prefiere (1: drama, 2: romántico, 3: acción, 4: suspenso, 5: terror). La lectura
@@ -8,6 +8,6 @@ finaliza cuando llega una persona con DNI 33555444, la cual debe procesarse.
 Una vez finalizada la lectura de todas las personas, se pide:
 a. Informar la cantidad de personas cuyo DNI contiene más dígitos pares que impares.
 b. Informar los dos códigos de género más elegidos.
-Cc. Realizar un módulo que reciba un DNI, lo busque y lo elimine de la estructura. El DNI puede no
+c. Realizar un módulo que reciba un DNI, lo busque y lo elimine de la estructura. El DNI puede no
 existir. Invocar dicho módulo en el programa principal.
 }
