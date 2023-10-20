0822203ba315e468a77d3594bf6645753434a2e8
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-20 16:32:17 -0300

    Create ejercicio4.pas

---
+++ Practica7/ejercicio4.pas
@@ -0,0 +1,7 @@
+{
+Una maternidad dispone información sobre sus pacientes. De cada una se conoce: nombre, apellido y
+peso registrado el primer día de cada semana de embarazo (a lo sumo 42). La maternidad necesita un
+programa que analice esta información, determine e informe:
+a. Para cada embarazada, la semana con mayor aumento de peso.
+b. El aumento de peso total de cada embarazada durante el embarazo.
+}
