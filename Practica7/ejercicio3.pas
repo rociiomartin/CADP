3c918ed78101a8fea141281ada9020c93888f42e
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-20 16:31:59 -0300

    Create ejercicio3.pas

---
+++ Practica7/ejercicio3.pas
@@ -0,0 +1,8 @@
+{
+3. Una remisería dispone de información acerca de los viajes realizados durante el mes de mayo de
+2020. De cada viaje se conoce: número de viaje, código de auto, dirección de origen, dirección de
+destino y kilómetros recorridos durante el viaje. Esta información se encuentra ordenada por código
+de auto y para un mismo código de auto pueden existir 1 o más viajes. Se pide:
+
+a. Informar los dos códigos de auto que más kilómetros recorrieron.
+}
