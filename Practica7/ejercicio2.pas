63f8a17b2ad5bc8effaf5b2d131438f030997df4
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-20 16:30:07 -0300

    Create ejercicio2.pas

---
+++ Practica7/ejercicio2.pas
@@ -0,0 +1,16 @@
+{
+Implementar un programa que lea y almacene información de clientes de una empresa aseguradora
+automotriz. De cada cliente se lee: código de cliente, DNI, apellido, nombre, código de póliza
+contratada (1..6) y monto básico que abona mensualmente. La lectura finaliza cuando llega el cliente
+con código 1122, el cual debe procesarse.
+
+La empresa dispone de una tabla donde guarda un valor que representa un monto adicional que el
+cliente debe abonar en la liquidación mensual de su seguro, de acuerdo al código de póliza que tiene
+contratada.
+Una vez finalizada la lectura de todos los clientes, se pide:
+a. Informar para cada cliente DNI, apellido, nombre y el monto completo que paga mensualmente
+por su seguro automotriz (monto básico + monto adicional).
+b. Informar apellido y nombre de aquellos clientes cuyo DNI contiene al menos dos dígitos 9.
+Cc. Realizar un módulo que reciba un código de cliente, lo busque (seguro existe) y lo elimine de la
+estructura.
+}
