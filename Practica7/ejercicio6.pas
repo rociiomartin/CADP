5ac4ba3055f477bba1545e6cd9dbf3ca98f6c4e6
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-20 16:38:50 -0300

    Create ejercicio6.pas

---
+++ Practica7/ejercicio6.pas
@@ -0,0 +1,21 @@
+{
+El Observatorio Astronómico de La Plata ha realizado un relevamiento sobre los distintos objetos
+astronómicos observados durante el año 2015. Los objetos se clasifican en 7 categorías: 1: estrellas,
+2: planetas, 3: satélites, 4: galaxias, 5: asteroides, 6: cometas y 7: nebulosas.
+
+Al observar un objeto, se registran los siguientes datos: código del objeto, categoría del objeto (1..7),
+nombre del objeto, distancia a la Tierra (medida en años luz), nombre del descubridor y año de su
+descubrimiento.
+
+A. Desarrolle un programa que lea y almacene la información de los objetos que han sido
+observados. Dicha información se lee hasta encontrar un objeto con código -1 (el cual no
+debe procesarse). La estructura generada debe mantener el orden en que fueron leídos los
+datos.
+B. Una vez leídos y almacenados todos los datos, se pide relizar un reporte con la siguiente
+información:
+1. Los códigos de los dos objetos más lejanos de la tierra que se hayan observado.
+2. La cantidad de planetas descubiertos por "Galileo Galilei" antes del año 1600.
+3. La cantidad de objetos observados por cada categoría.
+4. Los nombres de las estrellas cuyos códigos de objeto poseen más dígitos pares que
+impares.
+}
