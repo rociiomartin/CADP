46e2ceca6776973d6cbc390b6bf9ced98d553599
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-30 14:21:38 -0300

    Create Parcial1.pas

---
+++ Parciales/Parcial1.pas
@@ -0,0 +1,17 @@
+{
+﻿CADP 2019 CON AVVAY DC 5000 Posic.
+Segundo recuperatorio - COMPLETO
+19/7/2019
+Una editorial vende libros de apoyo escolar para varias materias, codificadas de la siguiente forma: 1) Historia; 2) Geografía; 3) Matemática; 4) Biología; 5) Física; 6) Química y 7) Lengua. El catálogo de la editorial consta de 5000 libros con la siguiente información: código de libro (entre 1 y 5000), código de materia (entre 1 y 7), título, año de edición, precio de venta
+y apellido del autor. Lis ta...
+La editorial dispone de una estructura de datos con información de todas las ventas realizadas. De cada venta se conoce el código del libro, el DNI del cliente y el mes y año de la venta.
+Realizar un programa que:
+5000
+00 De 5000 Post PSP. No necesiton CONTROLAY
+1. Lea y almacene la información de los todos los libros del catálogo de la editorial. Los libros son ingresados sin ningún orden particular.
+2. Una vez leído y almacenado el catálogo de libros, procese la información de las ventas (que dispone la editorial), calcule e informe: 1,.7
+a. Las dos materias con mayor cantidad de libros vendidos.
+ob. Para las ventas a clientes con DNI sin dígitos pares, en las cuales el libro vendido fue editado entre los años
+2011 y 2017, informar el año de la venta y el título del libro vendido.
+c. El porcentaje de ventas en las que el precio del libro vendido superó los $2000.
+}
