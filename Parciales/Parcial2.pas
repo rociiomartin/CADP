d997c235dd970d1ca34e7d1adf3390315752fb96
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-30 14:20:36 -0300

    Create Parcial2.pas

---
+++ Parciales/Parcial2.pas
@@ -0,0 +1,13 @@
+{
+﻿Redictado CADP 2021 - Parcial - 2da fecha
+Una compañía de telefonía celular dispone de una estructura de datos con información de todas las facturas emitidas durante los años 2019 y 2020 para sus 3400 clientes. De cada factura se conoce el mes (1 a 12), el año (2019 6 2020), el código del cliente y el monto facturado. La información se encuentra ordenada por código del cliente.
+a.
+Parentin
+Realizar un módulo que retorne en una estructura de datos la información de los clientes. De cada cliente se conoce su código (entre 1 y 3400), nombre y apellido, DNI y localidad. La información se ingresa por teclado sin ningún orden en particular.
+b. Realizar un módulo que reciba la información de las facturas, la información de los clientes y un código de cliente (que podría no existir), y retorne una estructura de datos con nombre, apellido, DNI del cliente, monto total facturado al cliente, y mes, año y monto de cada una de las facturas emitidas al cliente. Esta información debe estar ordenada por monto de las facturas.
+se refiere
+小
+Nota: incluir el programa principal donde se invoquen los módulos implementados.
+Imm
+R
+}
