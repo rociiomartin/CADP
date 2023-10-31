c97581e551492420de63ee43abb0db19d1cb879e
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-10-31 16:09:52 -0300

    Update Parcial2.pas

--- Parciales/Parcial2.pas
+++ Parciales/Parcial2.pas
@@ -1,19 +1,6 @@
 {
 REDICTADO DE CADP 2019 - Parcial 3ra Fecha (17/12/2019) - COMPLETO
 Un comercio de la ciudad de La Plata necesita un programa para administrar información de las ventas realizadas durante 2019. Para ello, se debe:
-A) Leer y almacenar la información de las ventas. De cada venta se lee: código, fecha (día y mes), DNI del cliente y monto. La lectura de información finaliza cuando se lee el DNI -1 (que no debe procesarse). La información debe quedar almacenada de manera ordenada por DNI del cliente. Tener en cuenta que cada cliente puede aparecer en una o más ventas.
-B) Una vez leída y almacenada toda la información, calcular e informar:
-1. El monto total gastado por cada cliente.
-2. Meses en los cuales se realizaron mayor y menor cantidad de ventas el primer día del mes.
-3. Porcentaje de ventas realizadas entre Febrero y Septiembre y con código compuesto por la misma cantidad de dígitos pares que impares.
-C) Implementar un módulo que elimine, de la estructura generada en "A)", la venta con código recibido por parámetro. Tener en cuenta que la venta puede no existir. El módulo debe retornar si pudo o no eliminarse la
-venta.
-NOTA: Se pide realizar la invocación del módulo.
-}
-
-{
-REDICTADO DE CADP 2019 - Parcial 3ra Fecha (17/12/2019) - COMPLETO
-Un comercio de la ciudad de La Plata necesita un programa para administrar información de las ventas realizadas durante 2019. Para ello, se debe:
 
 A) Leer y almacenar la información de las ventas. De cada venta se lee: código, fecha (día y mes), DNI del cliente y monto. La lectura de 
 información finaliza cuando se lee el DNI -1 (que no debe procesarse). La información debe quedar almacenada de manera ordenada por DNI del cliente. 
@@ -116,11 +103,11 @@ begin
 end;
 
 procedure RecorrerLista(l:lista; var vc:vectorContador);
-    procedure InicializarVector(var vectorContador);
+    procedure InicializarVector(var vc:vectorContador);
     var
         i:rangoM;
     begin
-        for i:= 1 to DF do v[i]:=0;
+        for i:= 1 to DF do vc[i]:=0;
     end;
     function Cumple(dni:integer):boolean;
     var
@@ -148,12 +135,12 @@ begin
         while ( l <> nil ) and (actual = l^.datos.dni ) do
         begin
             montoTotal:=montoTotal+ l^.datos.monto;
-            if (l^.datos.f.dia = 1)then v[l^.datos.f.mes]:=v[l^.datos.f.mes]+1;
-            if ( (l^.datos.f.mes = 2 ) and (l^.datos.f.mes = 9) and (Cumple (l^.datos.dni)) ) then cant:=cant+1;
+            if (l^.datos.f.dia = 1)then vc[l^.datos.f.mes]:=vc[l^.datos.f.mes]+1;
+            if ( ((l^.datos.f.mes = 2 ) or (l^.datos.f.mes = 9)) and (Cumple (l^.datos.dni)) ) then cant:=cant+1;
             cantT:=cantT+1;
             l:=l^.sig;
         end;  
-        writeln('El monto total gastado para el cliente ', actual, ' es: ', montoTotal);
+        writeln('El monto total gastado para el cliente ', actual, ' es: ', montoTotal:00:00);
     end;
     porcentaje:=(cant/cantT)*100;
     writeln('Porcentaje de ventas realizadas entre Febrero y Septiembre y con código compuesto por la misma cantidad de dígitos pares que impares: ', porcentaje:00:00,'%');
@@ -165,7 +152,7 @@ procedure RecorrerVector(vc:vectorContador);
         if (cant > max)then
         begin
             max:=cant;
-            mes:=mes1;
+            mes1:=mes;
         end;
     end;
     procedure Minimo(var mes1,min:integer; mes, cant:integer);
@@ -173,7 +160,7 @@ procedure RecorrerVector(vc:vectorContador);
         if (cant < min)then
         begin
             min:=cant;
-            mes:=mes1;
+            mes1:=mes;
         end;
     end;
 var
@@ -183,20 +170,49 @@ begin
     max:=-1; min:=999;
     for i:=1 to DF do 
     begin
-        Maximo(mes1,max,i,v[i]);
-        Minimo(mes2,min,i,v[i]);
+        Maximo(mes1,max,i,vc[i]);
+        Minimo(mes2,min,i,vc[i]);
     end;
     writeln('Se realizaron cantidad de ventas el primer día del mes: ');
     writeln (' Mes mayor: ' , mes1);
     writeln (' Mes menor: ' , mes2);
 end;
+
+ procedure Eliminar (Var l: lista; cod:integer;var ok:boolean);
+var
+    actual,ant:lista;
+begin
+    actual:=l; 
+    ok:=true;
+    while (actual <> nil) and (ok) do
+    begin
+        if (actual^.datos.cod <> cod) then
+        begin
+            ant:=actual;
+            actual:= actual^.sig;
+        end
+        else 
+        begin
+          if (actual = l) then l:= l^.sig 
+                          else ant^.sig:= actual^.sig;
+          dispose (actual);
+          ok:=false;
+          actual:= ant;
+        end;
+    end;
+end;
 //PP
 var
     l:lista;
     vc:vectorContador;
+    cod:integer;
+    ok:boolean;
 begin
     l:=nil;
     CargarLista(l);
     RecorrerLista(l,vc);
     RecorrerVector(vc);
+    writeln('Ingrese un codigo a eliminar'); readln(cod);
+    Eliminar(l,cod,ok);
+    if (not ok) then writeln('Se borro');
 end.
