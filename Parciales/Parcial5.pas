eea58a86c486a1058183404de4c7319ddc8d8b99
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-01 15:20:35 -0300

    Create Parcial5.pas

---
+++ Parciales/Parcial5.pas
@@ -0,0 +1,11 @@
+{
+Un restaurante necesita un programa para administrar la información de los platos que ofrece. Se dispone de una estructura con la 
+información de los platos. De cada plato se conoce: nombre, precio, categoría (1: Entrada, 2: Principal, 3: Postre, 4: Minuta), 
+cantidad de ingredientes y nombre de cada uno de los ingredientes (a lo sumo 10).
+
+Se pide:
+
+A) Generar una nueva estructura con nombre y precio de cada plato que posea "perejil" entre sus ingredientes.
+B) Informar las dos categorías con mayor cantidad de platos ofrecidos.
+C) Informar el precio promedio de los platos con más de 5 ingredientes.
+}
