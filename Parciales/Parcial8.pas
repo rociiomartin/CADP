eea80d9467082a51610bef009b387d8541dbf301
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-07 17:00:45 -0300

    Create Parcial8.pas

---
+++ Parciales/Parcial8.pas
@@ -0,0 +1,18 @@
+{
+CADP 2023 - Parcial - Primera Fecha - 10/6/2023 - TEMA 1 10:30 hs
+
+Una empresa de venta de tickets de tren está analizando la información de los viajes realizados por sus trenes
+durante el año 2022. Para ello, se dispone de una estructura de datos con la información de todos los viajes.
+De cada viaje se conoce el código de tren, el mes en que se realizó el viaje (entre 1 y 12), la cantidad de
+pasajeros que viajaron, y el código de la ciudad de-destino (entre 1 y 20). La información se encuentra
+ordenada por código de tren.
+
+Además, la empresa dispone de una estructura de datos con información del costo del ticket por ciudad
+destino.
+
+Realizar un programa que procese la información de los viajes y:
+A. Genere una lista que tenga por cada código del tren, a cantidad de viajes realizados
+B. Informe el mes con mayor monto recaudado. :
+C. COMPLETO: Informe el promedio de pasajeros. por cada tren.entre fodos sus e
+
+}
