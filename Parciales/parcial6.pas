bf33302d2222d34d7fa8183a5cbfd9a726567b45
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-24 18:21:03 +0000

    Create parcial6.pas

---
+++ Parciales/parcial6.pas
@@ -0,0 +1,10 @@
+{
+
+Una juguetería necesita un programa para administrar la información de los productos que comercializa. De cada producto se lee
+código, descripción, categoría (1: Primera infancia, 2: Didácticos, 3: De mesa, 4: Otros), marca y precio. La lectura finaliza 
+cuando se lee el código -1, que no debe procesarse. La información se lee ordenada por marca.
+Se pide:
+A) Generar una estructura que contenga, para cada marca, la cantidad de productos comercializados. 
+B) Informar las dos categorías con menor cantidad de productos comercializados.
+C) Informar el precio promedio de los productos con código múltiplo de 3.
+}
