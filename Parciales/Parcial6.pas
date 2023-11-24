ce3a24d2fd596770e4370612dad59e81019466d4
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-24 18:21:27 +0000

    Update and rename parcial6.pas to Parcial6.pas

--- Parciales/Parcial6.pas
+++ Parciales/Parcial6.pas
@@ -1,5 +1,5 @@
 {
-
+REDICTADO CADP 2023 (31/10),TEMA 1
 Una juguetería necesita un programa para administrar la información de los productos que comercializa. De cada producto se lee
 código, descripción, categoría (1: Primera infancia, 2: Didácticos, 3: De mesa, 4: Otros), marca y precio. La lectura finaliza 
 cuando se lee el código -1, que no debe procesarse. La información se lee ordenada por marca.
