f3b02161b9d862da64fbfb3ef1dce12b846a4c04
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-29 18:30:48 +0000

    Update Parcial10.pas

--- Parciales/Parcial10.pas
+++ Parciales/Parcial10.pas
@@ -1,11 +1,13 @@
 {
+CADP 2023 - Parcial - Segunfa Fecha - 25/11/2023 
 Un salón de eventos necesita un programa para administrar la información de las reservas de diciembre de 2023. Se dispone de 
 una estructura con la información de las reservas. De cada reserva se conoce: número de reserva, DNI del cliente, día del
 evento (1..31), hora de inicio, hora de fin y categoría de servicio (1..4). Además, se dispone de una tabla con el precio por 
 hora de reserva de acuerdo a cada categoría de servicio.
 Se pide:
 
-A) Generar una nueva estructura con número y precio total de cada reserva. Esta estructura debe generarse ordenada por el precio total de cada reserva.
+A) Generar una nueva estructura con número y precio total de cada reserva. Esta estructura debe generarse ordenada por el 
+precio total de cada reserva.
 B) Informar los dos días del mes con menor cantidad de reservas de clientes con DNI impar.
 C) Informar el porcentaje de reservas de eventos que inicien después de las 12 hs y se produzcan en la segunda quincena.
 }
