a0eb8016f8663c1f286619198e2b80a335c0eb8a
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-30 18:15:20 +0000

    Update Parcial9.pas

--- Parciales/Parcial9.pas
+++ Parciales/Parcial9.pas
@@ -1,9 +1,13 @@
 {
-Un salón de eventos necesita un programa para administrar la información de las reservas de diciembre de 2023. Se dispone de una estructura con la información de las reservas. De cada reserva se conoce: número de reserva, DNI del cliente, día del evento (1..31), hora de inicio, hora de fin y categoría de servicio (1..4). Además, se dispone de una tabla con el precio por hora de reserva de acuerdo a cada categoría de servicio.
-
+CADP 2023 - Parcial - Segunfa Fecha - 25/11/2023 
+Un salón de eventos necesita un programa para administrar la información de las reservas de diciembre de 2023. Se dispone de una 
+estructura con la información de las reservas. De cada reserva se conoce: número de reserva, DNI del cliente, día del evento (1..31),
+hora de inicio, hora de fin y categoría de servicio (1..4). Además, se dispone de una tabla con el precio por hora de reserva de 
+acuerdo a cada categoría de servicio.
 Se pide:
 
-A) Generar una nueva estructura con número y precio total de cada reserva. Esta estructura debe generarse ordenada por número de reserva.
+A) Generar una nueva estructura con número y precio total de cada reserva. Esta estructura debe generarse ordenada por número de 
+reserva.
 B) Informar los dos días del mes con mayor cantidad de reservas de clientes con DNI par.
 C) Informar el porcentaje de reservas de eventos que inicien antes de las 12 hs y se produzcan en la primera quincena.
 }
