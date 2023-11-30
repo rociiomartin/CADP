2b0e7cd0d2fe27c3922d0773c7f465f720f60f27
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-30 18:47:44 +0000

    Update Parcial2.pas

--- Parciales/2019/Parcial2.pas
+++ Parciales/2019/Parcial2.pas
@@ -1,15 +1,17 @@
 {
 REDICTADO DE CADP 2019 - Parcial 3ra Fecha (17/12/2019) - COMPLETO
-Un comercio de la ciudad de La Plata necesita un programa para administrar información de las ventas realizadas durante 2019. Para ello, se debe:
+Un comercio de la ciudad de La Plata necesita un programa para administrar información de las ventas realizadas durante 2019. 
+Para ello, se debe:
 
-A) Leer y almacenar la información de las ventas. De cada venta se lee: código, fecha (día y mes), DNI del cliente y monto. La lectura de 
-información finaliza cuando se lee el DNI -1 (que no debe procesarse). La información debe quedar almacenada de manera ordenada por DNI del cliente. 
-Tener en cuenta que cada cliente puede aparecer en una o más ventas.
+A) Leer y almacenar la información de las ventas. De cada venta se lee: código, fecha (día y mes), DNI del cliente y monto. La 
+lectura de información finaliza cuando se lee el DNI -1 (que no debe procesarse). La información debe quedar almacenada de 
+manera ordenada por DNI del cliente. Tener en cuenta que cada cliente puede aparecer en una o más ventas.
 
 B) Una vez leída y almacenada toda la información, calcular e informar:
 1. El monto total gastado por cada cliente.
 2. Meses en los cuales se realizaron mayor y menor cantidad de ventas el primer día del mes.
-3. Porcentaje de ventas realizadas entre Febrero y Septiembre y con código compuesto por la misma cantidad de dígitos pares que impares.
+3. Porcentaje de ventas realizadas entre Febrero y Septiembre y con código compuesto por la misma cantidad de dígitos pares que
+impares.
 
 C) Implementar un módulo que elimine, de la estructura generada en "A)", la venta con código recibido por parámetro. Tener en cuenta que la venta puede 
 no existir. El módulo debe retornar si pudo o no eliminarse la venta.
