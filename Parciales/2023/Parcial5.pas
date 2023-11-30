b9d41a97b35cc8c93f68321bb18e73e8dd0d9276
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-30 19:00:46 +0000

    Update Parcial5.pas

--- Parciales/2023/Parcial5.pas
+++ Parciales/2023/Parcial5.pas
@@ -1,5 +1,5 @@
 {
-REDICTADO CADP 2023 (31/10),TEMA 1
+CADP 2023 (31/10),TEMA 1
 Un restaurante necesita un programa para administrar la información de los platos que ofrece. Se dispone de una estructura con la 
 información de los platos. De cada plato se conoce: nombre, precio, categoría (1: Entrada, 2: Principal, 3: Postre, 4: Minuta), 
 cantidad de ingredientes y nombre de cada uno de los ingredientes (a lo sumo 10).
