e31712f84f5b115cc26456b7f8b17502861278a7
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-30 19:38:01 +0000

    Update Parcial5.pas

--- Parciales/2023/CursadaNormal/Parcial5.pas
+++ Parciales/2023/CursadaNormal/Parcial5.pas
@@ -4,8 +4,7 @@
 El productor musical Bizarrap organiza sesiones musicales con diferentes artistas. Se dispone de una estructura de datos con la
 información de las sesiones de Bizarrap. De cada sesión se conoce: título, nombre del artista, género musical (1: Trap Latino, 
 2: Reggaeton 3: Urban, 4: Electrónica, 5: Pop Rap), año de estreno y cantidad de reproducciones en Spotify. La información no 
-posee un orden específico.
-Procesar la información anterior, para:
+posee un orden específico.Procesar la información anterior, para:
 
 A. Informar los dos códigos de género musical con menor cantidad de reproducciones en Spotify.
 B. Generar una lista ordenada por año que contenga las sesiones del género "Trap Latino" y "Urban" cuya suma de los dígitos
