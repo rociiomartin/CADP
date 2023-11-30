3acfb2edc854a6f148e61ab3b96c9276405c6ad1
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-30 19:25:48 +0000

    Create Parcial3.pas

---
+++ Parciales/2023/CursadaNormal/Parcial3.pas
@@ -0,0 +1,15 @@
+{
+CADP 2023 - Parcial Primera Fecha - 10/6/2023 - TEMA 2 - 8:00 AM
+
+Una empresa de ventas de pasajes en colectivo está analizando la información de los viajes realizados a Mar del Plata en el último 
+año. Para ello, se dispone de una estructura de datos con la información de todos los viajes realizados. De cada viaje se conoce el
+código de viaje, el número de coche (entre 1000 y 2500), el mes en que se realizó el viaje, cantidad de pasajes vendidos y el dni 
+del chófer. La información 'no se encuentra ordenada por ningún criterio. Además, la empresa dispone de una estructura de datos con
+información sobre la capacidad máxima de cada coche.
+Realizar un programa que procese la información de los viajes e:
+
+A. Informe el número de coche con el cual se realizaron más viajes.
+B. Genere una lista con los viajes realizados en el mes 2 por un chofer con dni múltiplo de 10, donde la cantidad de pasajes
+vendidos no alcanza la capacidad máxima del coche.
+C. COMPLETO: Para cada coche, informe el promedio de pasajeros que viajaron entre todos sus viajes.
+}
