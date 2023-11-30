4333c99e05d489c7a86e4681c68fee11dc0f45ef
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-30 19:13:40 +0000

    Create Parcial5.pas

---
+++ Parciales/2023/CursadaNormal/Parcial5.pas
@@ -0,0 +1,14 @@
+{
+CADP 2023 - Parcial Tercera Fecha - 15/7/2023 - TEMA 1
+Una revista deportiva dispone de información de los jugadores de fútbol participantes de la liga profesional 2022. De cada jugador se
+conoce código de jugador, apellido y nombres, código de equipo (1..28), año de nacimiento y la calificación obtenida para cada una de
+las 27 fechas del torneo ya finalizado. La calificación es numérica de 0 a 10, donde el valor 0 significa que el jugador no ha 
+participado de la fecha. 
+Se solicita:
+
+a. Informar para cada equipo la cantidad de jugadores mayores a 35 años. 
+b. Informar los códigos de los 2 jugadores con mejor calificación promedio en los partidos en los que participó. Solo deben 
+considerarse los jugadores que participaron en más de 14 fechas.
+c. (COMPLETO): Implementar e invocar a un módulo que genere una lista con los jugadores cuyo código posee exactamente 3 dígitos 
+impares y haya nacido entre 1983 y 1990. La lista debe estar ordenada por código de jugador. 
+}
