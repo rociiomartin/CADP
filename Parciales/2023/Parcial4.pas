0a8d60fe5bc41bb2dfbc11d40af0f0b15f181596
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-30 19:04:27 +0000

    Create Parcial4.pas

---
+++ Parciales/2023/Parcial4.pas
@@ -0,0 +1,15 @@
+{
+﻿CADP 2023-Parcial - Segunda Fecha - 01/07/2023- TEMA 1-8:00 AM
+El productor musical Bizarrap organiza sesiones musicales con diferentes artistas. Implementar un programa que lea y almacene 
+información de dichas sesiones. De cada sesión se lee: título de sesión, nombre del artista, género musical (1: Trap Latino, 2: 
+Reggaeton 3: Urban, 4: Electrónica, 5: Pop Rap) y cantidad de visualizaciones en YouTube. La lectura finaliza cuando se lee la 
+sesión del artista con nombre "Peso Pluma", la cual debe procesarse. La información debe quedar almacenada en una estructura de 
+datos ordenada por título de sesión de forma ascendente.
+Una vez leída y almacenada la información, se pide:
+
+A. Informar los dos códigos de género musical con mayor cantidad de visualizaciones en YouTube.
+B. Informar la cantidad de sesiones del género "Reggaeton" cuya cantidad de visualizaciones contiene la misma cantidad de dígitos
+pares que impares.
+C. COMPLETO: Realizar un módulo que reciba un título de sesión y elimine dicha sesión de la estructura. El título de sesión puede 
+no existir. Invocar el módulo desarrollado en el programa principal con un título leído por teclado.
+}
