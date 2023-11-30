01848474f252495cb675d18c094f7ef160cf12cd
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-30 19:15:24 +0000

    Create Parcial4.pas

---
+++ Parciales/2023/CursadaNormal/Parcial4.pas
@@ -0,0 +1,14 @@
+{
+﻿CADP 2023-Parcial - Segunda Fecha - 01/07/2023 - TEMA 2 - 8:00 AM
+El productor musical Bizarrap organiza sesiones musicales con diferentes artistas. Se dispone de una estructura de datos con la
+información de las sesiones de Bizarrap. De cada sesión se conoce: título, nombre del artista, género musical (1: Trap Latino, 
+2: Reggaeton 3: Urban, 4: Electrónica, 5: Pop Rap), año de estreno y cantidad de reproducciones en Spotify. La información no 
+posee un orden específico.
+Procesar la información anterior, para:
+
+A. Informar los dos códigos de género musical con menor cantidad de reproducciones en Spotify.
+B. Generar una lista ordenada por año que contenga las sesiones del género "Trap Latino" y "Urban" cuya suma de los dígitos
+de la cantidad de reproducciones sea múltiplo de 5.
+C. COMPLETO: Realizar un módulo que reciba la lista generada en el punto anterior e informe para cada año, la cantidad de 
+sesiones realizadas junto al total de reproducciones de Spotify de esas sesiones.
+}
