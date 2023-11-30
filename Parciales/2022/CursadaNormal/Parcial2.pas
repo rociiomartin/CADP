6da152bf1e43e777ed2c472c04e6bd42717b43fc
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-30 19:46:34 +0000

    Create Parcial2.pas

---
+++ Parciales/2022/CursadaNormal/Parcial2.pas
@@ -0,0 +1,18 @@
+{
+CADP 2022 - TEMA 1 - COMPLETO 02/07/2022
+La cátedra de CADP necesita un programa para generar el listado de alumnos ingresantes que rendirán el parcial. Para ello, la cátedra
+DISPONE de un listado con todos alumnos que ingresaron este año a la facultad. De cada alumno se conoce su DNI, nombre y apellido,
+nota obtenida en curso de ingreso (entre 4 y 10), turno (entre 1 y 4) y si estuvo presente o ausente en cada una de las 12 clases 
+de práctica.
+
+a) Realizar un módulo que reciba la información de los alumnos y retorne una nueva estructura de datos que contenga sólo aquellos
+alumnos que podrán rendir el parcial. Para poder rendir el parcial, los alumnos deben contar con al menos 8 asistencias en las 12
+clases de práctica.
+
+b) Realizar un módulo que reciba la estructura de datos generada en el inciso anterior, e IMPRIMA en pantalla: 
+1. Apellido y nombre y el DNI de los alumnos que hayan obtenido nota 8 o superior en el ingreso.
+2. Turno con mayor cantidad de alumnos en condiciones de rendir el examen.
+3. Cantidad de alumnos que no posean ningún dígito cero en su DNI.
+
+NOTA: Implementar el programa principal.
+}
