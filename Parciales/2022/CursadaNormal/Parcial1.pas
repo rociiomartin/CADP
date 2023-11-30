9aee69defca5010b9e1649de2bf899e439b6b237
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2023-11-30 19:42:34 +0000

    Create Parcial1.pas

---
+++ Parciales/2022/CursadaNormal/Parcial1.pas
@@ -0,0 +1,18 @@
+{
+﻿CADP 2022 - TEMA 1 - COMPLETO 11/06/2022
+ 
+Una panadería artesanal de La Plata vende productos de elaboración propia. La panadería agrupa a sus productos en 26 categorías
+(por ej.: 1. Pan; 2. Medialunas; 3. Masas finas; etc.). Para cada categoría se conoce su nombre y el precio por kilo del producto.
+La panadería dispone de la información de todas las compras realizadas en el último año. De cada compra se conoce el DNI del cliente,
+la categoría del producto (entre 1 26) y la cantidad de kilos comprados. La información se encuentra ordenada por DNI del cliente.
+
+a) Realizar un módulo que retorne la información de las categorías en una estructura de datos adecuada. La información se lee por 
+teclado y sin ningún orden en particular. De cada categoría se lee el nombre, el código (1 a 26) y el precio por kilo.
+
+b) Realizar un módulo que reciba la información de todas las compras, la información de las categorías, y retorne:
+1. DNI del cliente que más compras ha realizado.
+2. Monto total recaudado por cada categoría
+3. Cantidad total de compras de clientes con DNI compuesto por, al menos, 3 dígitos pares.
+
+NOTA: Implementar el programa principal.
+}
