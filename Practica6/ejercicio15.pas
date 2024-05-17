1fe14960336c7865489a5c50df99fba3970efdf3
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2024-05-17 16:11:27 -0300

    Create ejercicio15.pas

---
+++ Practica6/ejercicio15.pas
@@ -0,0 +1,11 @@
+{
+La cátedra de CADP está organizando la cursada para el año 2019. Para ello, dispone de una lista con
+todos los alumnos que cursaron EPA. De cada alumno se conoce su DNI, apellido, nombre y la nota
+obtenida. Escribir un programa que procese la información de alumnos disponible y los distribuya en
+turnos utilizando los siguientes criterios:
+- Los alumnos que obtuvieron al menos 8 en EPA deberán ir a los turnos 1 ó 4.
+- Los alumnos que obtuvieron entre 5 y 8 deberán ir a los turnos 2, 3 ó 5.
+- Los alumnos que no alcanzaron la nota 5 no se les asignará turno en CADP.
+Al finalizar, el programa debe imprimir en pantalla la lista de alumnos para cada turno.
+Nota: La distribución de alumnos debe ser lo más equitativa posible.
+}
