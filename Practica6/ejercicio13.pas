130bd2a210cbb8fac11abe72a3a721edafb306a1
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2024-05-17 16:10:33 -0300

    Create ejercicio13.pas

---
+++ Practica6/ejercicio13.pas
@@ -0,0 +1,11 @@
+{
+El Portal de Revistas de la UNLP está estudiando el uso de sus sistemas de edición electrónica por parte
+de los usuarios. Para ello, se dispone de información sobre los 3600 usuarios que utilizan el portal. De
+cada usuario se conoce su email, su rol (1: Editor; 2. Autor; 3. Revisor; 4. Lector), revista en la que
+participa y cantidad de días desde el último acceso.
+a. Imprimir el nombre de usuario y la cantidad de días desde el último acceso de todos los usuarios
+de la revista Económica. El listado debe ordenarse a partir de la cantidad de días desde el último
+acceso (orden ascendente).
+b. Informar la cantidad de usuarios por cada rol para todas las revistas del portal.
+c. Informar los emails de los dos usuarios que hace más tiempo que no ingresan al portal.
+}
