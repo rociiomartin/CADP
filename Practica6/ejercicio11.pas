5bc7a083f242d72b23e4f4031abf196f6db6402a
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2024-05-17 16:09:57 -0300

    Create ejercicio11.pas

---
+++ Practica6/ejercicio11.pas
@@ -0,0 +1,10 @@
+{
+La Facultad de Informática debe seleccionar los 10 egresados con mejor promedio a los que la UNLP les
+entregará el premio Joaquín V. González. De cada egresado se conoce su número de alumno, apellido y
+el promedio obtenido durante toda su carrera.
+mplementar un programa que:
+a. Lea la información de todos los egresados, hasta ingresar el código 0, el cual no debe procesarse.
+b. Una vez ingresada la información de los egresados, informe el apellido y número de alumno de los
+egresados que recibirán el premio. La información debe imprimirse ordenada según el promedio
+del egresado (de mayor a menor).
+}
