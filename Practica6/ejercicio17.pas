3dae6bd627a489eb2fb8577ed77f6c35b2810435
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2024-05-17 16:12:15 -0300

    Create ejercicio17.pas

---
+++ Practica6/ejercicio17.pas
@@ -0,0 +1,9 @@
+{
+Continuando con los 3 ejercicios adicionales de la Guía opcional de actividades adicionales, ahora
+sumaremos lo aprendido sobre listas para almacenar la información ingresada por teclado.
+Consideraciones importantes:
+● Los datos ingresados por teclado deberán almacenarse en una estructura de tipo lista apropiada.
+● Una vez leídos y almacenados los datos, deberán procesarse (recorrer la lista) para resolver cada
+inciso. Al hacerlo, deberán reutilizarse los módulos ya implementados en las prácticas anteriores.
+En la medida de lo posible, la lista deberá recorrerse una única vez para resolver todos los inciso
+}
