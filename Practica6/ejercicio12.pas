27bb58873752abd7480f5bc0a094aa7a5d462fb9
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2024-05-17 16:10:14 -0300

    Create ejercicio12.pas

---
+++ Practica6/ejercicio12.pas
@@ -0,0 +1,10 @@
+{
+Una empresa desarrolladora de juegos para teléfonos celulares con Android dispone de información de
+todos los dispositivos que poseen sus juegos instalados. De cada dispositivo se conoce la versión de
+Android instalada, el tamaño de la pantalla (en pulgadas) y la cantidad de memoria RAM que posee
+(medida en GB). La información disponible se encuentra ordenada por versión de Android. Realizar un
+programa que procese la información disponible de todos los dispositivos e informe:
+a. La cantidad de dispositivos para cada versión de Android.
+b. La cantidad de dispositivos con más de 3 GB de memoria y pantallas de a lo sumo a 5 pulgadas.
+c. El tamaño promedio de las pantallas de todos los dispositivos.
+}
