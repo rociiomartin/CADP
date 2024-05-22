23dd2c98b28038f6d8ca5dfa46df5d02a6b5abbc
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2024-05-22 19:52:11 -0300

    Update ejercicio8.pas

--- Practica4/ejercicio8.pas
+++ Practica4/ejercicio8.pas
@@ -1,1 +1,97 @@
+{
 
+8. Realizar un programa que lea y almacene la información de 400 alumnos ingresantes a la Facultad de
+Informática de la UNLP en el año 2020. De cada alumno se lee: nro de inscripción, DNI, apellido,
+nombre y año de nacimiento. Una vez leída y almacenada toda la información, calcular e informar:
+a. El porcentaje de alumnos con DNI compuesto sólo por dígitos pares.
+b. Apellido y nombre de los dos alumnos de mayor edad
+}
+
+program ocho;
+type
+	alumno = record
+		nroinsc: integer;
+		dni: integer;
+		nombre: string;
+		apellido: string;
+		anionac: integer;
+	end;
+	vecalumnos = array[1..400] of alumno;
+
+procedure leeralumno(var a: alumno);
+begin
+	readln(a.nroinsc);
+	readln(a.nombre);
+	readln(a.apellido);
+	readln(a.dni);
+	readln(a.anionac);
+end;
+
+procedure cargarvector(var va: vecalumnos);
+var
+	i: integer;
+	a: alumno;
+begin
+	for i:= 1 to 400 do begin
+		leeralumno(a);
+		va[i]:= a;
+	end;
+end;
+
+function descomponer(dni: integer): boolean;
+var
+	dig: integer; 
+	ok: boolean;
+begin
+	ok:= true;
+	while(dni <> 0) and (ok) do begin
+		dig:= dni mod 10;
+		if (dig mod 2 <> 0) then
+			ok:= false;
+		dni:= dni div 10;
+	end;
+	descomponer:= ok;
+end;
+
+procedure sacarmaximos(a: alumno; var max1,max2: integer; var apemax1,apemax2,nommax1,nommax2: string); 
+begin
+	if(a.anionac > max1) then begin
+		max2:= max1;
+		apemax2:= apemax1;
+		nommax2:= nommax1;
+		max1:= a.anionac;
+		nommax1:= a.nombre;
+		apemax1:= a.apellido;
+	end
+	else begin
+		if(a.anionac > max2) then begin
+			max2:= a.anionac;
+			nommax2:= a.nombre;
+			apemax2:= a.apellido;
+		end;
+	end;
+end;
+
+procedure recorrervector(va: vecalumnos; var por:integer; var apemax1,apemax2,nommax1,nommax2:string);
+var
+	i,cantpares,max1,max2: integer;
+begin
+	cantpares:= 0;
+	for i:= 1 to 400 do begin
+		if(descomponer(va[i].dni)) then
+			cantpares:= cantpares + 1;
+		sacarmaximos(va[i], max1,max2,apemax1,apemax2,nommax1,nommax2);
+	end;
+	por:=(cantpares * 100)/ 400;
+end;
+
+var
+	va: vecalumnos;
+	por:integer;
+	nommax1,nommax2,apemax1,apemax2: string;
+begin
+	cargarvector(va);
+	recorrervector(va,por);
+	writeln(por);
+	writeln(nommax1,apemax1,nommax2,apemax2);
+end.
