b0d259b56193e294b062e974a01dcbc30d84fddf
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2024-06-28 15:12:33 -0300

    Create ejercicio12.pas

---
+++ Practica7/ejercicio12.pas
@@ -0,0 +1,13 @@
+{
+El centro de deportes Fortaco’s quiere procesar la información de los 4 tipos de suscripciones que ofrece:
+1)Musculación, 2)Spinning, 3)Cross Fit, 4)Todas las clases. Para ello, el centro dispone de una tabla con
+información sobre el costo mensual de cada tipo de suscripción.
+Realizar un programa que lea y almacene la información de los clientes del centro. De cada cliente se conoce el
+nombre, DNI, edad y tipo de suscripción contratada (valor entre 1 y 4). Cada cliente tiene una sola suscripción.
+La lectura finaliza cuando se lee el cliente con DNI 0, el cual no debe procesarse.
+Una vez almacenados todos los datos, procesar la estructura de datos generada, calcular e informar:
+- La ganancia total de Fortaco’s
+- Las 2 suscripciones con más clientes.
+- Genere una lista con nombre y DNI de los clientes de más de 40 años que están suscritos a CrossFit o a
+Todas las clases. Esta lista debe estar ordenada por DNI.
+}
