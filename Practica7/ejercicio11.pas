836ea2247026ba8f7a5e7e534446820b3f320f44
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2024-06-28 15:12:08 -0300

    Create ejercicio11.pas

---
+++ Practica7/ejercicio11.pas
@@ -0,0 +1,15 @@
+{
+Realizar un programa para una empresa productora que necesita organizar 100 eventos culturales. De cada
+evento se dispone la siguiente información: nombre del evento, tipo de evento (1: música, 2: cine, 3: obra de
+teatro, 4: unipersonal y 5: monólogo), lugar del evento, cantidad máxima de personas permitidas para el evento
+y costo de la entrada. Se pide:
+1. Generar una estructura con las ventas de entradas para tales eventos culturales. De cada venta se debe
+guardar: código de venta, número de evento (1..100), DNI del comprador y cantidad de entradas
+adquiridas. La lectura de las ventas finaliza con código de venta -1.
+2. Una vez leída y almacenada la información de las ventas, calcular e informar:
+a. El nombre y lugar de los dos eventos que han tenido menos recaudación.
+b. La cantidad de entradas vendidas cuyo comprador contiene en su DNI más dígitos pares que impares
+y que sean para el evento de tipo “obra de teatro”.
+c. Si la cantidad de entradas vendidas para el evento número 50 alcanzó la cantidad máxima de
+personas permitidas.
+}
