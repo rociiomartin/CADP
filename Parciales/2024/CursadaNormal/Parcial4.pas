17e0383ff1f4fbd50e39bc9f0670fd3656ce3c11
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2024-07-01 16:19:27 -0300

    Create Parcial4.pas

---
+++ Parciales/2024/CursadaNormal/Parcial4.pas
@@ -0,0 +1,10 @@
+{
+TEMA 1 - CADP 2024 - 29/6/2024
+Un supermercado está procesando las compras que realizaron sus clientes. Para ello, dispone de una estructura de datos con todas las compras, sin ningún orden en particular. De cada compra se conoce código, mes, año (entre 2014 y 2024), monto y el DNI del cliente. Un cliente puede haber realizado más de una compra.
+Realizar un programa lea de teclado un año y luego:
+a. Almacene en otra estructura de datos las compras realizadas en el año leído. Esta estructura debe quedar ordenada por DNI del cliente.
+b. Una vez almacenada la información, procese los datos del inciso a. e informe:
+1. Para cada cliente, el monto total facturado entre todas sus compras.
+2. Los dos meses con menor facturación.
+3. COMPLETO: La cantidad de compras con código múltiplo de 10.
+}
