18d51bf29c76bb39b141426a0f17a8897dc6ebee
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2024-11-21 14:48:25 -0300

    Update Parcial1.pas

--- Parciales/2024/Redictado/Parcial1.pas
+++ Parciales/2024/Redictado/Parcial1.pas
@@ -1,1 +1,8 @@
-
+Una fábrica de automóviles necesita un programa para administrar las, 900000 piezas de los diferentes modelos de automóviles que fabrica. De cada pieza, se conoce, código de pieza, descripción, cantidad en stock, costo de la pieza, código de modelo al que pertenece (1 al 10), siock mínimo y su peso en kg.
+A. Leer y almacenar la información de las piezas en una estructura de datos adecuada. La información debe quedar almacenada en el mismo orden que fue leída, → iNSERTAR ADELANTE
+B. A partir de la estructura generada en A. se requiere:
+1. Realizar una lista de las piezas cuyo stock actual sea Igual al stock mínimo.
+2. Calcular el promedio de piezas que pesan más de 1 kg y su código de pieza posea al menos 3 dígitos
+múltiplos de 2.
+Utilizando la lista generace en B) 1. Cafcolar e ifermar los dos codigos de pieras ris costosas cuyo
+codigo de modeio sea inenor que 5.
