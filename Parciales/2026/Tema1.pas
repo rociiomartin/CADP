baed8cdeeb9cb7784215ce7b781f1a740a6fc9b7
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2026-09-04 08:29:15 -0300

    Crear Tema1.pas

---
+++ Parciales/2026/Tema1.pas
@@ -0,0 +1,10 @@
+{
+TEMA 1 -CADP 2026
+4/7/2026
+la biblioteca de la Universidad Nacional de La Plata necesita un programa para administrar información de préstamos de libros efectuados en marzo de 2020. Para ello, se debe leer la información de los préstamos realizados. De cada préstamo se lee: nro. de préstamo, ISBN del libro prestado, nro. de socio al que se prestó el libro, día del préstamo (1..31). La información de los préstamos se lee de manera ordenada por ISBN y finaliza cuando se ingresa el ISBN -1 (que no debe procesarse). Se pide:
+a) Generar una estructura que contenga, para cada ISBN de libro, la cantidad de veces que fue prestado. Esta estructura debe
+quedar ordenada por ISBN de libro.
+Calcular el día del mes en que se realizaron menos préstamos.
+c) Calcular el porcentaje de préstamos que poseen nro. de préstamo impar y nro. de socio par.
+NOTA: Una vez completados los incisos, imprimir la estructura generada en el inciso A y los datos obtenidos en los incisos B y C.
+}
